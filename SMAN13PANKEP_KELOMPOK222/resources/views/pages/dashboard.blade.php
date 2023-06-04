8b5b1ded8609bf05c170b4607bef7ae369417741
adsetyawan <setyawanaridwi@gmail.com>
2023-06-04 22:39:10 +0700

    [SP3_PBI30] Membuat fitur reporting resolve #92

--- SMAN13PANKEP_KELOMPOK222/resources/views/pages/dashboard.blade.php
+++ SMAN13PANKEP_KELOMPOK222/resources/views/pages/dashboard.blade.php
@@ -2,25 +2,64 @@
 @section('content')
 <div class="container">
     <div class="row">
+        <a href="{{ route('cetak-dashboard') }}" target="_blank" class="btn btn-primary mb-4"><i class="fas fa-plus"></i>
+            Cetak Data
+        </a>
         <div class="col-md-3 mb-3">
-            <a href="{{ route('siswa.index') }}" class="text-decoration-none text-dark">
+            <a href="{{ route('users.index') }}" class="text-decoration-none text-dark">
                 <div class="card">
                     <div class="card-body">
                         <div class="text-center">
-                            <div class="display-3">{{ $count['siswa'] }}</div>
-                            <div class="fs-6 mt-3">Siswa</div>
+                            <div class="display-3">{{ $count['user'] }}</div>
+                            <div class="fs-6 mt-3">Jumlah Pengguna</div>
                         </div>
                     </div>
                 </div>
             </a>
         </div>
         <div class="col-md-3 mb-3">
-            <a href="{{ route('users.index') }}" class="text-decoration-none text-dark">
+            <a href="{{ route('siswa.index') }}" class="text-decoration-none text-dark">
                 <div class="card">
                     <div class="card-body">
                         <div class="text-center">
-                            <div class="display-3">{{ $count['guru'] }}</div>
-                            <div class="fs-6 mt-3">Guru</div>
+                            <div class="display-3">{{ $count['siswa'] }}</div>
+                            <div class="fs-6 mt-3">Jumlah Siswa</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <!-- <div class="col-md-3 mb-3">
+            <a href="{{ route('siswa.index') }}" class="text-decoration-none text-dark">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <div class="display-3">{{ $count['pangkep'] }}</div>
+                            <div class="fs-6 mt-3">SMAN 13 PANGKEP</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div> -->
+        <div class="col-md-3 mb-3">
+            <a href="{{ route('pembayaran-siswa.index') }}" class="text-decoration-none text-dark">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <div class="display-3">{{ $count['pembayaransiswa'] }}</div>
+                            <div class="fs-6 mt-3">Jumlah Transaksi</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md-3 mb-3">
+            <a href="{{ route('metode-pembayaran.index') }}" class="text-decoration-none text-dark">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <div class="display-3">{{ $count['metode'] }}</div>
+                            <div class="fs-6 mt-3">Metode Pembayaran</div>
                         </div>
                     </div>
                 </div>
@@ -32,7 +71,7 @@
                     <div class="card-body">
                         <div class="text-center">
                             <div class="display-3">{{ $count['buku'] }}</div>
-                            <div class="fs-6 mt-3">Buku</div>
+                            <div class="fs-6 mt-3">Jumlah Katalog Buku</div>
                         </div>
                     </div>
                 </div>
@@ -44,7 +83,7 @@
                     <div class="card-body">
                         <div class="text-center">
                             <div class="display-3">{{ $count['ekstrakulikuler'] }}</div>
-                            <div class="fs-6 mt-3">Ekstrakulikuler</div>
+                            <div class="fs-6 mt-3">Informasi Ekstrakulikuler</div>
                         </div>
                     </div>
                 </div>
@@ -56,13 +95,100 @@
                     <div class="card-body">
                         <div class="text-center">
                             <div class="display-3">{{ $count['aktivitas'] }}</div>
-                            <div class="fs-6 mt-3">Kegiatan</div>
+                            <div class="fs-6 mt-3">Informasi Aktivitas</div>
                         </div>
                     </div>
                 </div>
             </a>
         </div>
-
+        <div class="col-md-3 mb-3">
+            <a href="{{ route('peminjaman-buku.index') }}" class="text-decoration-none text-dark">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <div class="display-3">{{ $count['peminjamanbuku'] }}</div>
+                            <div class="fs-6 mt-3">Peminjaman Buku</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md-3 mb-3">
+            <div class="card">
+                <div class="card-header">Level Peminjaman Buku</div>
+                <div class="card-body">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>Rendah</td>
+                                <td>{{ $bookLevels['Low'] }}</td>
+                            </tr>
+                            <tr>
+                                <td>Tinggi</td>
+                                <td>{{ $bookLevels['Medium'] }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div> 
+        <div class="col-md-3 mb-3">
+            <div class="card">
+                <div class="card-header">Indikator Jumlah Pengguna</div>
+                <div class="card-body">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>Rendah</td>
+                                <td>{{ $penggunaLevels['Low'] }}</td>
+                            </tr>
+                            <tr>
+                                <td>Tinggi</td>
+                                <td>{{ $penggunaLevels['Medium'] }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div> 
+        <div class="col-md-3 mb-3">
+            <div class="card">
+                <div class="card-header">Indikator Jumlah Katalog Buku</div>
+                <div class="card-body">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>Rendah</td>
+                                <td>{{ $bukuLevels['Low'] }}</td>
+                            </tr>
+                            <tr>
+                                <td>Tinggi</td>
+                                <td>{{ $bukuLevels['Medium'] }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div> 
+        <div class="col-md-3 mb-3">
+            <div class="card">
+                <div class="card-header">Indikator Jumlah Pembayaran</div>
+                <div class="card-body">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>Rendah</td>
+                                <td>{{ $pembayaranLevels['Low'] }}</td>
+                            </tr>
+                            <tr>
+                                <td>Tinggi</td>
+                                <td>{{ $pembayaranLevels['Medium'] }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div> 
     </div>
 </div>
 @endsection
